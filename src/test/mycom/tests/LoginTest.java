19298f790aa3e671e7e4bbc38105a58ec09cf9d1
promo888 <promo888@gmail.com>
2015-09-06 15:15:26 +0300

    create project

---
+++ src/test/mycom/tests/LoginTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import com.infra.AbstractCucumberTest;
+import com.infra.annotation.Feature;
+import com.infra.annotation.Scenario;
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Igor
+ * Date: 9/6/15
+ * Time: 2:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Feature("login")
+public class LoginTest extends AbstractCucumberTest {
+
+    @Test(threadPoolSize = 5)
+    @Scenario("Login with guest account")
+    public void loginWithGuestAccount() {
+        run();
+    }
+}
